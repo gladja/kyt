d3adca32190e779e15117d1ddf87fb12331a2f53
gladja <gladkih.alexander@gmail.com>
2024-07-05 20:26:26 +0300

    refactor header hero

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,6 @@
-// const overlayForm = document.querySelector('[data-overlay]');
-// const overlayHero = document.querySelector('.hero__wrap');
+// burger menu
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
-// const openFormBtn = document.querySelector('.js-open-form');
-// const closeFormBtn = document.querySelector('.js-close-form');
 
 window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
   if (!e.matches) return;
@@ -12,4 +9,18 @@ window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
 
 openMenuBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('is__open');
+});
+
+// scroll
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    header.style.top = '-150px';
+  } else {
+    header.style.top = '0';
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
---
+++ src/js/modal.js
@@ -0,0 +1,60 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', event => {
+  // Отримати всі кнопки, які відкривають модальне вікно
+  const openModalButtons = document.querySelectorAll('.services-btn');
+
+  // Отримати всі кнопки, які закривають модальне вікно
+  const closeButtons = document.querySelectorAll('.close');
+
+  // Додати подію "click" для відкриття модального вікна
+  openModalButtons.forEach(button => {
+    button.addEventListener('click', event => {
+      event.preventDefault(); // Запобігає переходу за посиланням
+      const modalId = event.target.getAttribute('data-modal');
+      const modal = document.getElementById(modalId);
+      modal.classList.add('show');
+      document.body.style.overflow = 'hidden'; // Заблокувати прокрутку сторінки
+      setTimeout(() => {
+        modal.style.display = 'block';
+      }, 10); // Невелика затримка для запуску переходу
+    });
+  });
+
+  // Додати подію "click" для закриття модального вікна
+  closeButtons.forEach(button => {
+    button.addEventListener('click', event => {
+      const modalId = event.target.getAttribute('data-modal');
+      const modal = document.getElementById(modalId);
+      modal.classList.remove('show');
+      document.body.style.overflow = ''; // Відновити прокрутку сторінки
+      setTimeout(() => {
+        modal.style.display = 'none';
+      }, 300); // Затримка відповідає тривалості переходу
+    });
+  });
+
+  // Закрити модальне вікно, якщо користувач натискає поза ним
+  window.addEventListener('click', event => {
+    if (event.target.classList.contains('modal')) {
+      event.target.classList.remove('show');
+      document.body.style.overflow = ''; // Відновити прокрутку сторінки
+      setTimeout(() => {
+        event.target.style.display = 'none';
+      }, 300); // Затримка відповідає тривалості переходу
+    }
+  });
+
+  // Закрити модальне вікно, якщо користувач натискає клавішу ESC
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape' || event.key === 'Esc') {
+      const modals = document.querySelectorAll('.modal');
+      modals.forEach(modal => {
+        modal.classList.remove('show');
+        document.body.style.overflow = ''; // Відновити прокрутку сторінки
+        setTimeout(() => {
+          modal.style.display = 'none';
+        }, 300); // Затримка відповідає тривалості переходу
+      });
+    }
+  });
+});
