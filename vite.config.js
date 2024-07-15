1d7af5fefc921c4e51c1772579e9d719b8b40bef
gladja <gladkih.alexander@gmail.com>
2024-07-15 22:57:26 +0300

    add package.json build2, add vite.config plugin add dot before /

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,35 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
+// plagin add dot before
+function addDotToPaths() {
+  return {
+    name: 'add-dot-to-paths',
+    enforce: 'post',
+    apply: 'build',
+    transformIndexHtml(html) {
+      const transformedHtml = html
+        .replace(/(src|href)="\/([^"]*)"/g, '$1="./$2"')
+        .replace(/url\((\/[^)]*)\)/g, 'url(./$1)');
+      console.log('Transformed HTML:', transformedHtml);
+      return transformedHtml;
+    },
+    generateBundle(options, bundle) {
+      for (const [fileName, asset] of Object.entries(bundle)) {
+        if (asset.type === 'asset' && asset.source) {
+          if (fileName.endsWith('.css') || fileName.endsWith('.js') || fileName.endsWith('.html')) {
+            let source = asset.source.toString();
+            source = source.replace(/url\((\/[^)]*)\)/g, 'url(./$1)');
+            source = source.replace(/(src|href)="\/([^"]*)"/g, '$1="./$2"');
+            console.log(`Transformed ${fileName}:`, source);
+            asset.source = source;
+          }
+        }
+      }
+    },
+  };
+}
+
 export default defineConfig({
   root: 'src',
   build: {
@@ -11,7 +40,11 @@ export default defineConfig({
     },
     outDir: '../dist',
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+    addDotToPaths(), // add custom plugin
+  ],
   optimizeDeps: {
     exclude: ['js-big-decimal'],
   },
