b8ae623be6ab8e72281debd06ebce98fbbbcc984
gladja <gladkih.alexander@gmail.com>
2024-11-11 22:49:45 +0200

    add button video

--- src/js/video.js
+++ src/js/video.js
@@ -1,11 +1,26 @@
 
-const video = document.getElementById("myVideo");
+const video = document.getElementById('myVideo');
+const toggleSoundButton = document.getElementById('toggleSound');
+const togglePlayButton = document.getElementById('togglePlay');
 
-window.addEventListener("scroll", () => {
-  const videoPosition = video.getBoundingClientRect();
-  if (videoPosition.top >= 0 && videoPosition.bottom <= window.innerHeight) {
+// Обработчик для включения/выключения звука
+toggleSoundButton.addEventListener('click', () => {
+  if (video.muted) {
+    video.muted = false;
+    toggleSoundButton.innerHTML = '<i class="fas fa-volume-up"></i>'; // Значок включенного звука
+  } else {
+    video.muted = true;
+    toggleSoundButton.innerHTML = '<i class="fas fa-volume-mute"></i>'; // Значок выключенного звука
+  }
+});
+
+// Обработчик для паузы/воспроизведения
+togglePlayButton.addEventListener('click', () => {
+  if (video.paused) {
     video.play();
+    togglePlayButton.innerHTML = '<i class="fas fa-pause"></i>'; // Значок паузы
   } else {
     video.pause();
+    togglePlayButton.innerHTML = '<i class="fas fa-play"></i>'; // Значок воспроизведения
   }
 });
