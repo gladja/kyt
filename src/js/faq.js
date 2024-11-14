144d557304269042dcd2146715ccc61f21ccac0c
gladja <gladkih.alexander@gmail.com>
2024-11-14 20:50:18 +0200

    refactor modal

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,17 +5,14 @@ document.querySelectorAll('.accordion__header').forEach(header => {
 
     if (openItem && openItem !== accordionContent) {
       openItem.style.maxHeight = 0;
-      // openItem.style.padding = '0 1rem';
       openItem.classList.remove('open');
       openItem.previousElementSibling.classList.remove('active');
     }
 
     if (accordionContent.classList.contains('open')) {
       accordionContent.style.maxHeight = 0;
-      // accordionContent.style.padding = '0 1rem';
     } else {
       accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-      // accordionContent.style.padding = '1rem';
     }
 
     accordionContent.classList.toggle('open');
--- src/js/video.js
+++ src/js/video.js
@@ -3,24 +3,22 @@ const video = document.getElementById('myVideo');
 const toggleSoundButton = document.getElementById('toggleSound');
 const togglePlayButton = document.getElementById('togglePlay');
 
-// Обработчик для включения/выключения звука
 toggleSoundButton.addEventListener('click', () => {
   if (video.muted) {
     video.muted = false;
-    toggleSoundButton.innerHTML = '<i class="fas fa-volume-up"></i>'; // Значок включенного звука
+    toggleSoundButton.innerHTML = '<i class="fas fa-volume-up"></i>'; 
   } else {
     video.muted = true;
-    toggleSoundButton.innerHTML = '<i class="fas fa-volume-mute"></i>'; // Значок выключенного звука
+    toggleSoundButton.innerHTML = '<i class="fas fa-volume-mute"></i>'; 
   }
 });
 
-// Обработчик для паузы/воспроизведения
 togglePlayButton.addEventListener('click', () => {
   if (video.paused) {
     video.play();
-    togglePlayButton.innerHTML = '<i class="fas fa-pause"></i>'; // Значок паузы
+    togglePlayButton.innerHTML = '<i class="fas fa-pause"></i>'; 
   } else {
     video.pause();
-    togglePlayButton.innerHTML = '<i class="fas fa-play"></i>'; // Значок воспроизведения
+    togglePlayButton.innerHTML = '<i class="fas fa-play"></i>'; 
   }
 });
