b8136d905dc4055e7a0999c66d36eb4703ef0696
gladja <gladkih.alexander@gmail.com>
2024-11-16 15:38:23 +0200

    refactor map.js

--- src/js/map.js
+++ src/js/map.js
@@ -1,26 +1,26 @@
-const button = document.getElementById('toggleButton');
-
-// add class active button: "next location" to btn color to stay yellow
-  button.addEventListener('mousedown', () => {
-    button.classList.add('active'); 
-  });
-
-
 // const button = document.getElementById('toggleButton');
 
-// // Функция для добавления класса active
-// function activateButton() {
-//   button.classList.add('active');
-// }
+// // add class active button: "next location" to btn color to stay yellow
+//   button.addEventListener('mousedown', () => {
+//     button.classList.add('active'); 
+//   });
+
+
+const button = document.getElementById('toggleButton');
+
+// Функция для добавления класса active
+function activateButton() {
+  button.classList.add('active');
+}
 
 // // // Функция для удаления класса active
 // // function deactivateButton() {
 // //   button.classList.remove('active');
 // // }
 
-// // События для ПК и мобильных устройств
-// button.addEventListener('mousedown', activateButton); // ПК
-// button.addEventListener('touchstart', activateButton); // Мобильные
+// События для ПК и мобильных устройств
+button.addEventListener('mousedown', activateButton); // ПК
+button.addEventListener('touchstart', activateButton); // Мобильные
 
 // // button.addEventListener('mouseup', deactivateButton); // ПК
 // // button.addEventListener('touchend', deactivateButton); // Мобильные
