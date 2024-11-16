4202c8ea2c7f47e5c5ffb21c33855c2d930abf99
gladja <gladkih.alexander@gmail.com>
2024-11-16 15:35:09 +0200

    refactor style map btn

--- src/js/map.js
+++ src/js/map.js
@@ -4,3 +4,27 @@ const button = document.getElementById('toggleButton');
   button.addEventListener('mousedown', () => {
     button.classList.add('active'); 
   });
+
+
+// const button = document.getElementById('toggleButton');
+
+// // Функция для добавления класса active
+// function activateButton() {
+//   button.classList.add('active');
+// }
+
+// // // Функция для удаления класса active
+// // function deactivateButton() {
+// //   button.classList.remove('active');
+// // }
+
+// // События для ПК и мобильных устройств
+// button.addEventListener('mousedown', activateButton); // ПК
+// button.addEventListener('touchstart', activateButton); // Мобильные
+
+// // button.addEventListener('mouseup', deactivateButton); // ПК
+// // button.addEventListener('touchend', deactivateButton); // Мобильные
+
+// // Если палец или курсор уходит с кнопки
+// // button.addEventListener('mouseleave', deactivateButton);
+// // button.addEventListener('touchcancel', deactivateButton);
