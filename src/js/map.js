776c20941005d0c969ac21dc206f3b5f69c489dc
gladja <gladkih.alexander@gmail.com>
2024-11-17 20:42:47 +0200

    change map.js

--- src/js/map.js
+++ src/js/map.js
@@ -1,37 +1,20 @@
+const button = document.getElementById('toggleButton');
+
+// add class active button: "next location" to btn color to stay yellow
+  button.addEventListener('mousedown', () => {
+    button.classList.add('active'); 
+  });
+
+
+
+
+
 // const button = document.getElementById('toggleButton');
 
-// // add class active button: "next location" to btn color to stay yellow
-//   button.addEventListener('mousedown', () => {
-//     button.classList.add('active'); 
-//   });
-
-
-// const button = document.getElementById('toggleButton');
-
-// // Добавляем класс active при нажатии
 // button.addEventListener('mousedown', () => {
 //   button.classList.add('active');
 // });
 
-// // Для мобильных устройств
-// // button.addEventListener('touchstart', () => {
-// //   button.classList.add('active');
-// // });
-
-
-
-// button.addEventListener('touchstart', (e) => {
-//     e.preventDefault(); // Предотвращаем дефолтное поведение
-//     button.classList.add('active');
-//   });
-
-
-const button = document.getElementById('toggleButton');
-
-button.addEventListener('mousedown', () => {
-  button.classList.add('active');
-});
-
-button.addEventListener('touchstart', () => {
-  button.classList.add('active');
-}, { passive: true });
+// button.addEventListener('touchstart', () => {
+//   button.classList.add('active');
+// }, { passive: true });
