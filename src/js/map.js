6093d59675610ff56baff22451a759f81854d65e
gladja <gladkih.alexander@gmail.com>
2024-11-17 20:47:10 +0200

    change map.js

--- src/js/map.js
+++ src/js/map.js
@@ -1,20 +1,6 @@
-const button = document.getElementById('toggleButton');
-
-// add class active button: "next location" to btn color to stay yellow
-  button.addEventListener('mousedown', () => {
-    button.classList.add('active'); 
-  });
-
-
-
-
-
 // const button = document.getElementById('toggleButton');
 
-// button.addEventListener('mousedown', () => {
-//   button.classList.add('active');
-// });
-
-// button.addEventListener('touchstart', () => {
-//   button.classList.add('active');
-// }, { passive: true });
+// // add class active button: "next location" to btn color to stay yellow
+//   button.addEventListener('mousedown', () => {
+//     button.classList.add('active'); 
+//   });
